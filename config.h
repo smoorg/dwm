c9df4c427a76f1f83ced3ae9a5ff951794de88f7
Mateusz Reszka <mateusz.reszka@outlook.com>
2020-12-29 23:44:26 +0100

    fonts changes

--- config.h
+++ config.h
@@ -4,10 +4,11 @@
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int gappx     = 15;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */static const int showbar            = 1;        /* 0 means no bar */
+static const int swallowfloating    = 1;        /* 1 means swallow floating windows by default */
+static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "NotoMono Nerd Font:size=12" };
-static const char dmenufont[]       = "NotoMono Nerd Font:size=12";
+static const char *fonts[]          = { "JetBrains Mono:size=13" };
+static const char dmenufont[]       = "JetBrains Mono:size=13";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -29,14 +30,16 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class    instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
-       { "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 },
-       { "eu4.exe",    NULL,     NULL,           0,         1,          0,           0,        -1 },
-       { "wine",    NULL,     NULL,           0,         1,          0,           0,        -1 },
-       { "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
-       { "St",      NULL,     NULL,           0,         0,          1,           0,        -1 },
-       { "termite", NULL,     NULL,           0,         0,          1,           0,        -1 },
-       { NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
+	/* class    	instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
+       { "Gimp",    	NULL,     NULL,           0,         1,          0,           0,        -1 },
+       { "eu4.exe",    	NULL,     NULL,           0,         1,          0,           0,        -1 },
+       { "wine",    	NULL,     NULL,           0,         1,          0,           0,        -1 },
+       { "BattleTech",  NULL,     NULL,           0,         1,          0,           0,        -1 },
+       { "steam",    	NULL,     NULL,           0,         1,          0,           0,        -1 },
+       { "Firefox", 	NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
+       { "St",      	NULL,     NULL,           0,         0,          1,           0,        -1 },
+       { "termite", 	NULL,     NULL,           0,         0,          1,           0,        -1 },
+       { NULL,      	NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 
 /* layout(s) */
